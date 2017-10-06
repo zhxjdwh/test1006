63bd2a1072f51c68247f623358c59c3dc1caa906
zhxjdwh <zhxjdwh@hotmail.com>
2017-10-07 01:45:05 +0800

    sds

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -27,6 +27,9 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
